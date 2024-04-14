b0a61b006b99638e8fe063150581efd4eed17796
tianhuzong <tianhuzong@github.com>
2024-04-14 15:55:27 +0800

    加载elementplus,开始编写页面

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<template >
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+   
+    <div>
+        <h1>hexoadmin 后台</h1>
+        <el-input v-model='http' style="max-width: 600px" placeholder="Please input" ><template #prepend>请输入服务器源地址：</template></el-input>  <el-input v-model='http' style="max-width: 600px" placeholder="Please input" ><template #prepend>请输入APIkey：</template></el-input>
+        <p>hello world</p>
+    </div>
+    
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang='ts'>
+import {ref} from 'vue'
+const http = ref('');
+</script>
